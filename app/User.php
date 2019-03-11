8412e7e4e570a214f326d0c309708397d890ba8d
Anh Quân <quanvuanh197@gmail.com>
2019-03-11 17:48:41 +0700

    firt-push

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','add','add2','as'
+        'name', 'email', 'password','add','add2','as','123'
     ];
 
     /**
