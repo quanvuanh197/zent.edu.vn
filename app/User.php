9b7f0b50247afdd65e31298d3f7dec8246d895a3
Anh Quân <quanvuanh197@gmail.com>
2019-03-11 17:38:55 +0700

    combine-2

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','add','add2','as','12123'
     ];
 
     /**
@@ -25,6 +25,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','token'
     ];
 }
